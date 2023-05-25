1d2ec2095e4477c1f81de568ab221671b75ec7aa
gdlbu <130327643+gdlbu@users.noreply.github.com>
2023-05-26 00:37:02 +0800

    Update ContentHeader.tsx

--- src/modules/Content/ContentHeader.tsx
+++ src/modules/Content/ContentHeader.tsx
@@ -37,9 +37,8 @@ const ContentHeader: FC<ContentHeaderProps> = ({
     >
       <div className="flex items-center flex-1 overflow-hidden mr-2">
         {isMobile ? (
-          <span className="text-3xl text-gradient font-[800]">GPT.TT
-            onClick={() => setCurrentId('')}
-          </span>            
+          <span className="text-3xl text-gradient font-[800]">GPT.TT</span>            
+          onClick={() => setCurrentId('')}                     
         ) : null}
         <div className="text-[#232629] flex-1 flex overflow-hidden">
           <div className="truncate">
